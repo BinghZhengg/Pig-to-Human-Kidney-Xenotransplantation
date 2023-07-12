f0dce56fcf7c42c6386ac0b026839bb7799bb4cd
Fi <87443621+wanqingpan@users.noreply.github.com>
2023-07-11 21:36:08 -0400

    Update 04_proliferating_population_analysis.R

--- scripts/04_proliferating_population_analysis.R
+++ scripts/04_proliferating_population_analysis.R
@@ -13,13 +13,8 @@ library(clusterProfiler)
 
 # Locate the Proliferaitng Cells on UMAP #
 # Figure 4 C
-DimPlot(kidneys,
-		  cols = c("#999999", "#299CA8", "#999999",
-		  			"#999999", "#999999","#999999",
-		  			"#999999", "#999999", "#999999", 
-		  			"#999999",
-		  			"#999999"))
-
+DimPlot(kidneys,cols = c("#999999", "#299CA8", "#999999", "#999999", "#999999","#999999",
+		  	 "#999999", "#999999", "#999999", "#999999", "#999999"))
 
 # Analyze this cluster separately
 s.genes <- cc.genes$s.genes
@@ -45,35 +40,35 @@ DefaultAssay(PT_Prolif) <- "SCT"
 
 # Figure 4 D
 FeaturePlot(PT_Prolif,
-				reduction = "umap",
-				features = c(
-					"STMN1",
-					"SLC34A1",
-					"CD3E"),
-				split.by = "Condition")
+reduction = "umap",
+features = c(
+	"STMN1",
+	"SLC34A1",
+	"CD3E"),
+split.by = "Condition")
 
 # Test how many cells are T cells
-PT_Prolif_T <- subset(PT_Prolif, CD3G > 1 | CD3E > 1)
+PT_Prolif_T <- subset(PT_Prolif, CD3G > 1 | CD3E > 1 | CD3D > 1)
 table(PT_Prolif_T$Condition)
 # kidney_1st_untreated_rt      kidney_1st_treated kidney_2nd_untreated_rt 
-# 0                       0                      68 
+# 0                       0                      69 
 # kidney_2nd_treated 
 # 0 
 
 # Look at cell cycle scoring
 # Figure 4 G
 DimPlot(PT_Prolif,
-		  reduction = "umap") + 
-			theme(axis.title = element_blank()) 
+	reduction = "umap") + 
+theme(axis.title = element_blank()) 
 
 # Look at organism assignment
 # Figure 4 H
 DimPlot(PT_Prolif,
-		  group.by = "origin") + theme(axis.title = element_blank(),
+	group.by = "origin") + theme(axis.title = element_blank(),
 		  													plot.title = element_blank())
 
 # Visualize top markers across cell cycle identities
 # Figure 4 I
 RidgePlot(PT_Prolif, 
-			 features = c("STMN1", "PCNA", "HMGN2", "HMGB2"), 
-			 ncol = 2)
+	  features = c("STMN1", "PCNA", "HMGN2", "HMGB2"), 
+	  ncol = 2)
